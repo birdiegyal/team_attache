b0a808ecac0718c2e8f8e2b8b947612328f30c07
birdiegyal <birdiegyal@gmail.com>
2024-03-04 14:45:21 +0530

    merging master to dev

--- src/lib/appwrite/config.ts
+++ src/lib/appwrite/config.ts
@@ -1,17 +1,19 @@
-import { Account, Avatars, Client, Databases, Teams } from "appwrite"
+import { Account, Avatars, Client, Databases, Storage, Teams } from "appwrite"
 
 export const appwriteConfig = {
     projectId: import.meta.env.VITE_PROJECT_ID,
     endpoint: import.meta.env.VITE_ENDPOINT,
     dbId: import.meta.env.VITE_DB_ID,
     usersColl: import.meta.env.VITE_USERS_COLL,
+    bucketId: import.meta.env.VITE_BUCKET_ID,
 }
 
 export const client = new Client()
     .setEndpoint(appwriteConfig.endpoint)
     .setProject(appwriteConfig.projectId)
-    
+
 export const teams = new Teams(client)
 export const account = new Account(client)
 export const databases = new Databases(client)
+export const storage = new Storage(client)
 export const avatars = new Avatars(client)
